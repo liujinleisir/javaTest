b1409f01f536205d92316d29a4aaf062f228d21e
liujinleisir <liujinlei_19921214@163.com>
2019-01-30 18:35:59 +0800

    zzz

--- src/test23designmode/结构型模式/桥梁模式/Test.java
+++ src/test23designmode/结构型模式/桥梁模式/Test.java
@@ -7,7 +7,7 @@ package test23designmode.结构型模式.桥梁模式;
  * @author liujinlei
  * @version 1.0
  */
-public class Test1 {
+public class Test{
     public static void main(String[] args) {
         Shape redCircle = new Circle(100,100, 10, new RedCircle());
         Shape greenCircle = new Circle(100,100, 10, new GreenCircle());
