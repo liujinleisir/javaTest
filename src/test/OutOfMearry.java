68de0543ef490c4888be466c36e58501adf1f4c6
liujl <liujl@oneecar.com>
2019-08-09 09:31:56 +0800

    测试提交

--- src/test/OutOfMearry.java
+++ src/test/OutOfMearry.java
@@ -1,5 +1,6 @@
 package test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,11 +10,16 @@ import java.util.List;
  * @create: 2019/8/7 9:59
  * @email: jinlei.liu@oneecar.com
  */
-public class OutOfMearry {
+public strictfp class OutOfMearry {
     public static void main(String args []){
-        List<String> list = new ArrayList<>();
-        while (true){
-            list.add("s");
-        }
+
+        BigDecimal a1 = new BigDecimal("1.2").subtract(new BigDecimal("1"));
+        BigDecimal b1 = new BigDecimal("0.2");
+        System.out.println(a1.compareTo(b1));
+        BigDecimal a2 = new BigDecimal(1.2).subtract(new BigDecimal(1));
+        BigDecimal b2 = new BigDecimal(0.2);
+        //
+        System.out.println(a2.compareTo(b2));
+
     }
 }
